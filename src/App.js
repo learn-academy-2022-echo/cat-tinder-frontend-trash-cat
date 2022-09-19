299f4f7ac3ccde68308dc06ddc9d3b1db74357d4
holdenprine <holdenprine404@gmail.com>
2022-09-19 16:42:17 -0700

    Files added - 14/20 completed, no problems atm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import RacoonIndex from './pages/RacoonIndex'
+import RacoonShow from './pages/RacoonShow'
+import RacoonNew from './pages/RacoonNew'
+import RacoonEdit from './pages/RacoonEdit'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Home />
+    <RacoonIndex />
+    <RacoonShow />
+    <RacoonNew />
+    <RacoonEdit />
+    <NotFound />
+    <Footer />
+
+    </>
   );
 }
 
